1e22670589433ccd15484ef76aa4deea20f14af3
Belcross17 <alfredochabes1@hotmail.com>
2022-07-11 18:36:23 -0400

    localstorage function created and ready to pull info after refrshing

---
+++ assets/js/javascript.js
@@ -0,0 +1,53 @@
+// global variables
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#1pm");
+var twoPm = $("#2pm");
+var threePm = $("#3pm");
+var fourPm = $("#4pm");
+var fivePm = $("#5pm");
+var todaysDate = moment().format('dddd') + "" + moment().format("Do MMM YYYY");
+var todaysHour = moment().format('h:mm:ss a');
+
+var hour = moment().hours();
+var userInput;
+var hourSpan;
+
+//function to set day and hour 
+var setInt = setInterval(function() {
+    var now = moment();
+    $('currentDay').html(now.format('YYYY MMMM DD') + '' + now.format('dddd') .substring(0,3).toUppercase());
+    $('currentDay').html(todaysDate + "" + now.format('hh:mm:ss A')); //check before submitting
+}, 100);
+
+//localStorage function
+function start() {
+    var start9 = JSON.parse(localStorage.getItem("09:00 AM"));
+    nineAm.val(start9);
+
+    var start10 = JSON.parse(localStorage.getItem("10:00 AM"));
+    tenAm.val(start10);
+
+    var start11 = JSON.parse(localStorage.getItem("11:00 AM"));
+    elevenAm.val(start11);
+
+    var start12 = JSON.parse(localStorage.getItem("12:00 PM"));
+    twelvePm.val(start12);
+
+    var start1 = JSON.parse(localStorage.getItem("01:00 PM"));
+    onePm.val(start1);
+
+    var start2 = JSON.parse(localStorage.getItem("02:00 PM"));
+    twoPm.val(start2);
+
+    var start3 = JSON.parse(localStorage.getItem("03:00 PM"));
+    threePm.val(start3);
+
+    var start4 = JSON.parse(localStorage.getItem("04:00 PM"));
+    fourPm.val(start4);
+
+    var start5 = JSON.parse(localStorage.getItem("05:00 PM"));
+    fivePm.val(start5);
+}
